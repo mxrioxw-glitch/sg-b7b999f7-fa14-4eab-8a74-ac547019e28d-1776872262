9dcf2de5c9239e5d1894d0d16921e44fb7091c46
softgenai <admin@softgen.ai>
2026-04-23 07:26:41 +0000

    fix(auth): remove erroneous business from super admin

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -12,4 +12,4 @@ export type Enums<T extends keyof Database['public']['Enums']> = Database['publi
 export type Functions<T extends keyof Database['public']['Functions']> = Database['public']['Functions'][T];
 
 // Project reference: qfpsvrumvcshswakcito
-// Last updated: 2026-04-23T07:25:44.680877
+// Last updated: 2026-04-23T07:26:33.975963
